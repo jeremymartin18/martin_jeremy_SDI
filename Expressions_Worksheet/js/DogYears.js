93ae8c6324aacd7cd85c59072eb58dff45e109f6
jeremymartin18 <jeremymartin18@gmail.com>
2015-09-09 21:31:11 -0400

    Finished Dog Years Calculations
    Finished Dog Years Calculations

--- Expressions_Worksheet/js/DogYears.js
+++ Expressions_Worksheet/js/DogYears.js
@@ -4,3 +4,12 @@ SDI Section 2
 Dog Years Practice Assignment
 09-08-2015
  */
+
+ //Define Variables
+ var age = 3;
+
+ //Calculate Sparky's age
+ var dogYears = age * 7;
+
+ //Output result to console.log
+ console.log("Sparky is " + age + " human years old, which is " + dogYears + " in dog years.");

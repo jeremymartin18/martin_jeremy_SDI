b0118d9d89e29a9fda27ecee448fdfa9a17c673a
jeremymartin18 <jeremymartin18@gmail.com>
2015-09-09 21:40:44 -0400

    Finished code for Slice of Pie 1
    Finished code for Slice of Pie 1

--- Expressions_Worksheet/js/SliceOfPie1.js
+++ Expressions_Worksheet/js/SliceOfPie1.js
@@ -4,3 +4,20 @@ SDI Section 2
 Slice of Pie 1 Practice Assignment
 09-08-2015
  */
+
+ //Define the variables
+
+ //Number of slices of pizza per pizza
+ var slices = 8;
+
+ //Number of people at the party
+ var people = 21;
+
+ //Number of pizzas ordered
+ var pizzas = 12;
+
+ //Calculate the number of slices of pizza per person
+ var slicesPerPerson = slices * pizzas / people;
+
+ //Output how many slices of pizza each person ate.
+ console.log("Each person ate " + slicesPerPerson + " slices of pizza at the party.");

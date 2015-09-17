171596909834c6debf1e3b9264f7b9dcb46e38b8
jeremymartin18 <jeremymartin18@gmail.com>
2015-09-16 21:27:47 -0400

    Setting prompts for calculator
    Coded prompts and set validation

--- Martin_Jeremy_Conditionals/js/martin_jeremy_conditionals.js
+++ Martin_Jeremy_Conditionals/js/martin_jeremy_conditionals.js
@@ -15,3 +15,17 @@ Fresh Beef 145
 Poultry 165
 Fish 145 OR clear flesh
  */
+
+ //Ask the user what type of meat they are cooking
+ var meatType = prompt("Let's make sure your meat is cooked to the proper temperature.\nWhat type of meat are you cooking? \n ground beef, fresh beef, poultry, or fish");
+
+ //Test to make sure the user did not leave the prompt blank
+ if(meatType === ""){
+     //Prompt the user to enter what type of meat they are cooking.
+     prompt("Please do not leave this blank.\nWhat type of meat are you cooking? \n ground beef, fresh beef, poultry, or fish");
+ }
+
+ //Ask the user what temperature the meat is currently at.
+ var currentTemp = prompt("Please enter the current temperature of the meat.");
+
+ //test to ensure the entered value is a number

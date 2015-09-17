13b07c7598b9f055bb9fe6d66a01008d202c5b2f
jeremymartin18 <jeremymartin18@gmail.com>
2015-09-16 22:42:50 -0400

    Changed Calculator
    Rewrote the layout of my calculator to be able to include all the
    elements of the assignment.

--- Martin_Jeremy_Conditionals/js/martin_jeremy_conditionals.js
+++ Martin_Jeremy_Conditionals/js/martin_jeremy_conditionals.js
@@ -8,49 +8,27 @@ Conditionals Assignment
  //alert("this is a test to see if connected");
 
 /*
-Meat Temperature Calculator
-
-Ground Beef 160
-Fresh Beef 145
-Poultry 165
-Fish 145 OR clear flesh
+Carpet price calculator
  */
 
- //Ask the user what type of meat they are cooking
- var meatType = prompt("Let's make sure your meat is cooked to the proper temperature.\nWhat type of meat are you cooking? \n ground beef, fresh beef, poultry, or fish");
+ //Ask the user what the length of the room is
 
- //Test to make sure the user did not leave the prompt blank
- if(meatType === ""){
-     //Prompt the user to enter what type of meat they are cooking.
-     prompt("Please do not leave this blank.\nWhat type of meat are you cooking? \n ground beef, fresh beef, poultry, or fish");
- }
+ //Validate that the value entered is a number
 
- //display variable in console
- console.log(" The meat type is " + meatType);
+ //Ask the user what the width of the room is
 
- //Ask the user what temperature the meat is currently at.
- var currentTemp = prompt("Please enter the current temperature of the meat.");
+ //Validate that the value entered is a number
 
- //Test to ensure the entered value is a number
- if(isNaN(currentTemp)){
-     //Reprompt the user to enter the temperature the meat is currently at.
-     currentTemp = prompt("Please only enter a number.\nPlease enter the current temperature of the meat.");
- }
+ //Ask the user what the regular price of the carpet is per square foot
 
- //display variable in console
- console.log("The current temp of the meat is " + currentTemp);
+ //Validate that the value entered is a number
 
- //Prompt user if flesh color of fish is clear or not
- if(meatType === "fish"){
-     var fishFlesh = prompt("Is the color of the flesh clear? (yes or no)");
+ //Ask the user what method of payment they will be using
 
-     //Reprompt user to enter yes or not for answer.
-     if(fishFlesh === !("yes") || !("no")){
-         fishFlesh = prompt("Please only enter 'yes' or 'no'. \nIs the color of the flesh clear? (yes or no)");
-     }
- }
+ //Validate that the user entered either "cash" or "credit"
 
- //display the variable in console
- console.log("The color of the fish flesh is " + fishFlesh);
+ //Calculate total square footage of the room and assign it to a variable
 
- 
+ //Determine the discount based on the size of the room
+
+ //Determine the discount based on the method of payment

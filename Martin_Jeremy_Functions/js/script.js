df2a80ae86a5d7fdccc92cb972cb8970ecfe314a
jeremymartin18 <jeremymartin18@gmail.com>
2015-09-23 21:36:12 -0400

    Tested and added comments
    Tested and added comments to the basic structure.

--- Martin_Jeremy_Functions/js/script.js
+++ Martin_Jeremy_Functions/js/script.js
@@ -1,24 +1,50 @@
 
  /*Jeremy Martin
 SDI Section 2
-Functions Assignment
+Functions Assignment - Pool Volume/Gallons Calculator
 09-23-2015
  */
 
  //alert ("this is a test to see if connected.");
 
+ //Declare and define a variable to prompt the user for the length of the pool
  var length = prompt("Lets find out how many gallons of water a pool will hold.\nWhat is the length of the pool rounded to the nearest foot?");
+
+ //Declare and define a variable to prompt the user for the width of the pool
  var width = prompt("What is the width of the pool rounded to the nearest foot?");
+
+ //Declare and define a variable to prompt the user for the average depth of the pool
  var depth = prompt("What is the average depth of the pool rounded to the nearest foot?");
+
+ //Declare a variable that will call the function to action and store the output of the variable.
+ //Will use the input from the user for the variables length, width, and depth as the arguments
  var totalVolume = volCalc (length, width, depth);
+
+ //Setup the function, name it, and define what will be used as the parameters for the arguments.
  function volCalc (l, w, d){
+
+     //Declare and define a variable to hold the calculated volume
      var volume = l * w * d;
+
+     //Return the output of the function back in to the main code and have it stored in the "totalVolume" variable.
      return volume;
  }
- console.log("The volume of the pool in cubic feet is " + totalVolume + ". \nNow let's figure out how many gallons of water that is.");
+
+ //Output to the user what the total volume of the pool is in cubic feet.
+ console.log("The volume of the pool in cubic feet is " + totalVolume + " cubic feet. \nNow let's figure out how many gallons of water is needed to fill that.");
+
+ //Setup the anonymous function with a v as the parameter
  var galCalc = function (v){
+
+     //Declare and define a variable to hold the calculated total number of gallons needed.
      var gallons = v * 7.5;
+
+     //Return the output of the function back in to the main code and have it stored in the "totalGallons" variable.
      return gallons;
  };
+
+ //Declare a variable that will call the function to action and store the output of the variable
  var totalGallons = galCalc(totalVolume);
+
+ //Output to the user what the total number of gallons needed to fill the pool.
  console.log("The total number of gallons of water needed to fill the pool is " + totalGallons + ".")

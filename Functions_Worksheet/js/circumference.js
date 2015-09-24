0ebc873d6d8716d993603604a918fba30f5e6fbf
jeremymartin18 <jeremymartin18@gmail.com>
2015-09-23 20:55:19 -0400

    Function for circumference
    Setup and tested the function for determining the circumference of a
    circle.

--- Functions_Worksheet/js/circumference.js
+++ Functions_Worksheet/js/circumference.js
@@ -4,3 +4,33 @@ SDI Section 2
 Circumference Practice Assignment
 09-22-2015
  */
+
+ /*
+  Calculate the circumference of a circle.
+
+  Parameter(s) for function:
+  Radius of the circle
+
+  Return:
+  Circumference of the circle
+
+  Result to print to the console:
+  “The circumference of the circle is X”;
+  */
+//declare and define the variable for the radius
+ var radius = 4;
+
+ //declare a variable and set it equal to the function call using the variable radius as the argument.
+ var circumference = calcCircum(radius);
+
+ //create the function named calcCircum and define the parameter "r"
+ function calcCircum (r){
+     //declare a variable to catch the calculation of the circumference of the circle.
+    var circ = 3.14 * r * r;
+
+     //return the calculationback to the main block of code.
+    return circ
+}
+
+ //output the result of the function.
+console.log("The circumference of the circle is " + circumference + ".");
--- Functions_Worksheet/js/stung.js
+++ Functions_Worksheet/js/stung.js
@@ -4,3 +4,19 @@ SDI Section 2
 Stung Practice Assignment
 09-22-2015
  */
+
+/*
+ It takes 8.666666667 bee stings per pound to kill an animal. Calculate how many bee stings are needed to kill an animal in a function
+
+ Given:
+ Victim’s weight (in pounds)
+
+ Parameter(s) for function:
+ Victim’s weight (in pounds)
+
+ Return:
+ Number of Bee stings
+
+ Result to print to the console:
+ “It takes X bee stings to kill this animal.
+ */
